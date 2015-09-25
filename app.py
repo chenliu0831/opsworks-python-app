051a227c0b4d711e2ef90049db31f10dd1919faa
Chenyang Liu <chenyang.liu@bodylabs.com>
2015-09-25 16:16:02 -0400

    Add another commit for testing deploy different revision

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ app = application = Flask(__name__)
 
 @app.route("/hi")
 def hello():
-    return "Hello World!"
+    return "Hello World V2!"
 
 if __name__ == "__main__":
-    print "Minial app for Poc Booting"
+    print "Minial app V2 for Poc Booting"
     app.run()
